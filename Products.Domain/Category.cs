1cb6803ac2f52bcda3dbc6f23ce58ec447740c47
Irving Herrera <irving.herreramolina@gmail.com>
2018-01-10 13:45:40 -0600

    add entity

---
+++ Products.Domain/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Products.Domain
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required.")]
+        [MaxLength(50, ErrorMessage = "The field {0} only can contain {1} characters length.")]
+        public int Description { get; set; }
+    }
+}
